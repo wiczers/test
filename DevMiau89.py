fdbfd6c3e30f7f1f85e9aa1e540055589e48023a
DevMiau89 <wieczorek.karolina1@o2.pl>
2017-06-17 21:03:08 +0200

    first version of DevMiau89 code

---
+++ DevMiau89.py
@@ -0,0 +1,15 @@
+print "Hello Troll, DevMiau89 here. Making some changes for fun"
+
+# This is fun. Try to figure this out. HueHue.
+def linear_search(lst, item):
+    i = 0
+    found = False
+    for i in range(len(lst)):
+        if lst[i] == item:
+            found = True
+            break
+        else:
+            i += 1
+    return found, i        
+
+print linear_search([11,23,58,31,56,77,43,12,65,19],19)             
