67e24d42d1d1fcce0b652a5528258c25918e991e
DevMiau89 <wieczorek.karolina1@o2.pl>
2017-06-17 20:49:04 +0200

    added hungry prompt

--- hungry.py
+++ hungry.py
@@ -1,1 +1,6 @@
-print "Koci is hungry!:P"
+hungry = raw_input("Is the cat hungry? Please enter yes or no :P)"
+
+if hungry  == "yes":
+    print "It is time for some snack."
+else:
+    print "write a code."
