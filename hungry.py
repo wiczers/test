76e412ab2386d1e28348eac6cda2e797355f629f
wiczers <jacek.wieczorek@poczta.onet.pl>
2017-06-17 23:32:44 +0200

    Update hungry.py

--- hungry.py
+++ hungry.py
@@ -3,4 +3,4 @@ hungry = raw_input("Is the cat hungry? Please enter yes or no :P)"
 if hungry  == "yes":
     print "It is time for some snack."
 else:
-    print "write a code."
+    print "go sleep under warm blanket."
